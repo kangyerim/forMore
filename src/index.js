9a1392e6a368058c8096832bb1246fc26021510e
강예림 <yerimkang521@gmail.com>
2022-11-20 13:38:35 +0900

    [REFACTOR] react-router 인덱스 따로 폴더 생성

--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AuthPage from "./pages/AuthPage";
 import store from "./store";
 import { Provider } from "react-redux";
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-	{
-		path: "/home",
-		element: <HomePage />,
-	},
-	{
-		path: "/auth",
-		element: <AuthPage />,
-	},
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
+		<RouterProvider router={router}>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</RouterProvider>
 	</React.StrictMode>
 );
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import AuthPage from "../pages/AuthPage";
+import App from "../App";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/home",
+		element: <HomePage />,
+	},
+	{
+		path: "/auth",
+		element: <AuthPage />,
+	},
+]);
+
+export default router;
