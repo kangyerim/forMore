e08ed6975fb20986d20e2fe866e41bbe4b7bbb81
강예림 <yerimkang521@gmail.com>
2022-12-01 21:44:21 +0900

    [Design] 랜딩 페이지 스타일링

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { authenSlice } from "../src/store/authenSlice";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
-	const [isUserLogin, setIsUserLogin] = useState(true);
-	// const userInfo = useSelector((state) => state.authen);
-	// console.log(userInfo);
+	const navigate = useNavigate();
+	const userInfo = useSelector((state) => state.authen);
+
+	useEffect(() => {
+		if (userInfo.uid) {
+			console.log(userInfo.displayName + " 로그인 상태 입니다.");
+			navigate("/home");
+		}
+	}, [userInfo]);
+
+	const onRouteToLogin = () => {
+		navigate("/login");
+	};
 
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">for More ( )</h1>
-			<button>
-				<Link to="/auth">login</Link>
-			</button>
+			<div className="grid grid-cols-1 justify-items-center my-80">
+				<h1 className="text-3xl font-bold pb-4">뽀모어()에 오신 것을 환영합니다!!</h1>
+				<button className="w-96 h-10 border-2 border-slate-300 rounded-lg mb-2" onClick={onRouteToLogin}>
+					뽀모어() 계정으로 시작하기
+				</button>
+				<div>
+					<span className="text-sm font-light">아직 계정이 없으신가요? </span>
+					<button className="text-sm font-medium">회원가입 하기</button>
+				</div>
+			</div>
 		</>
 	);
 };
--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
 		element: <HomePage />,
 	},
 	{
-		path: "/auth",
+		path: "/login",
 		element: <AuthPage />,
 	},
 ]);
