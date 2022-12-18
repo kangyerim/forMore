4157e93a272504489aa75ed23ecf586b7f888ec7
강예림 <yerimkang521@gmail.com>
2022-12-18 18:31:37 +0900

    [Feat] 회원가입 시, 이용약관 관련 체크박스 추가

--- src/components/Auth/SignUpForm.js
+++ src/components/Auth/SignUpForm.js
@@ -1,9 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 
 const SignUpForm = ({ onEnteredData }) => {
 	const emailInputRef = useRef();
 	const passwordInputRef = useRef();
 	const nickNameInputRef = useRef();
+
+	const [checkBoxInputContentList, setCheckBoxInputContentList] = useState([
+		{ id: "checkAllAgreement", text: " 모두 동의합니다.", isChecked: false, isBoldText: false },
+		{ id: "orderThan14", text: " 만 14세 이상입니다.", isChecked: false, isBoldText: false },
+		{ id: "agreeTermsOfUse", text: " 서비스 이용약관 동의", isChecked: false, isBoldText: true },
+		{ id: "collectionOfPersonalInfo", text: " 개인정보 수집 및 서비스 활용 동의", isChecked: false, isBoldText: true },
+	]);
 
 	const onSubmitHandler = (event) => {
 		event.preventDefault();
@@ -13,6 +20,37 @@ const SignUpForm = ({ onEnteredData }) => {
 		const currentNickName = nickNameInputRef.current.value;
 
 		onEnteredData({ email: currentEmail, password: currentPassword, nickName: currentNickName });
+	};
+
+	const onCheckboxHandler = (index) => {
+		let updatedCheckBoxes = [];
+		if (index === 0) {
+			const temp = checkBoxInputContentList.map((context) => {
+				context.isChecked = !context.isChecked;
+				return context;
+			});
+			updatedCheckBoxes.push(...temp);
+		} else {
+			const temp = checkBoxInputContentList.map((context, idx) => {
+				if (index === idx) context.isChecked = !context.isChecked;
+				if (!context.isChecked) checkBoxInputContentList[0].isChecked = false;
+				return context;
+			});
+			updatedCheckBoxes.push(...temp);
+		}
+
+		let checkedCnt = 0;
+		updatedCheckBoxes.forEach((context, idx) => {
+			if (idx > 0 && context.isChecked) checkedCnt++;
+		});
+
+		if (checkedCnt === 3) {
+			updatedCheckBoxes[0].isChecked = true;
+		} else if (1 > checkedCnt) {
+			updatedCheckBoxes[0].isChecked = false;
+		}
+
+		setCheckBoxInputContentList(updatedCheckBoxes);
 	};
 
 	return (
@@ -48,6 +86,18 @@ const SignUpForm = ({ onEnteredData }) => {
 						ref={passwordInputRef}
 						required
 					/>
+				</div>
+				<div>
+					<ul>
+						{checkBoxInputContentList.map(({ id, text, isChecked, isBoldText }, index) => {
+							return (
+								<li key={index}>
+									<input type="checkbox" id={id} checked={isChecked} onChange={() => onCheckboxHandler(index)} />
+									<label htmlFor={id}>{text}</label>
+								</li>
+							);
+						})}
+					</ul>
 				</div>
 
 				<button className="w-80 h-10  border-slate-300 rounded-md mb-2 my-4 text-white bg-black">회원가입</button>
