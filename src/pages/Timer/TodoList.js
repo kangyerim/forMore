41128fddc054a0a050af05c18c47e1cec1ea83b2
강예림 <yerimkang521@gmail.com>
2023-02-19 22:59:32 +0900

    [refactor] 첫번째 투두는 리스트렌더링 하지 않도록 변경

--- src/pages/Timer/TodoList.js
+++ src/pages/Timer/TodoList.js
@@ -6,54 +6,70 @@ import TimerPage from "./TimerPage";
 const TodoList = () => {
 	const { getTodoList } = useTodosCollection();
 	const userInfo = useSelector((state) => state.authenSlice);
+	const [clickedTodo, setClickedTodo] = useState({});
 	const [todoList, setTodoList] = useState([]);
+	const [isUpdateMode, setUpdateMode] = useState(false);
 
 	useEffect(() => {
 		if (userInfo.email) {
 			getTodoList().then((result) => {
+				const firstTodo = result[0];
+				setClickedTodo(() => {
+					return { ...firstTodo };
+				});
+				const restTodoList = result.splice(1, result.length);
 				setTodoList(() => {
-					return [...result, { title: "+", color: "grey", uid: String(new Date()) }];
+					return [...restTodoList, { title: "+", color: "grey", uid: String(new Date()) }];
 				});
 			});
 		}
 	}, [userInfo]);
 
 	const changeOrder = (event, index) => {
-		let currentList = todoList.splice(0, todoList.length - 1);
+		console.log("˚₊·—̳͟͞͞♡  event, index", event, index);
+		// let currentList = todoList.splice(0, todoList.length - 1);
 
-		let front = currentList.splice(index, currentList.length);
-		let back = currentList.splice(0, index);
+		// let front = currentList.splice(index, currentList.length);
+		// let back = currentList.splice(0, index);
 
-		setTodoList(() => {
-			return [...front, ...back, { title: "+", color: "grey", uid: String(new Date()) }];
-		});
+		// setTodoList(() => {
+		// 	return [...front, ...back, { title: "+", color: "grey", uid: String(new Date()) }];
+		// });
+	};
+
+	const changeMode = () => {
+		console.log("˚₊·—̳͟͞͞♡  changeMode");
 	};
 
 	return (
 		<>
 			<div className="flex items-center">
-				{todoList &&
-					todoList.map((todo, index) => {
-						return index === 0 ? (
-							<div className={`w-4/5 h-screen bg-${todo.color}-200 flex justify-center relative`} key={todo.uid}>
-								<h1 className="h-fit top-3 text-2xl font-bold align-middle absolute ">{todo.title}</h1>
-								<div className="absolute top-20">
-									{/* <TimerPage todo={todo} /> */}
-									<span className="text-2xl font-bold"> {todo.focusTime}/</span>
-									<span className="text-2xl font-bold">{todo.restTime}</span>
-								</div>
-							</div>
-						) : (
-							<div className={`w-20 h-screen bg-${todo.color}-200 flex items-center justify-center`} key={todo.uid}>
-								<h1
-									className="text-2xl font-bold align-middle whitespace-nowrap -rotate-90"
-									onClick={(e) => changeOrder(e, index)}
-								>
-									{todo.title}
-								</h1>
-							</div>
-						);
-					})}
+				<div className={`w-4/5 h-screen bg-${clickedTodo.color}-200 flex justify-center relative`}>
+					<h1 className="h-fit top-3 text-2xl font-bold align-middle absolute ">{clickedTodo.title}</h1>
+					<div className="absolute top-20">
+						<div>
+							<button onClick={changeMode}>수정</button>
+						</div>
+						<TimerPage todo={clickedTodo} />
+						<div>
+							<span className="text-2xl font-bold"> {clickedTodo.focusTime}/</span>
+							<span className="text-2xl font-bold">{clickedTodo.restTime}</span>
+						</div>
+					</div>
+				</div>
+
+				{todoList.map((todo, todoIndex) => {
+					return (
+						<div className={`w-20 h-screen bg-${todo.color}-200 flex items-center justify-center`} key={todoIndex}>
+							<h1
+								className="text-2xl font-bold align-middle whitespace-nowrap -rotate-90"
+								onClick={(e) => changeOrder(e, todoIndex)}
+							>
+								{todo.title}
+							</h1>
+						</div>
+					);
+				})}
 			</div>
 		</>
 	);
