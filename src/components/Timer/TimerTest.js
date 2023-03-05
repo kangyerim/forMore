b842d19eb6bf6c140ee34932b2a6f3ca52c1c81f
강예림 <yerimkang521@gmail.com>
2023-03-05 17:43:44 +0900

    [fix] 태그컬러 지정 시 group인풋 사용하도록 수정

--- src/components/Timer/TimerTest.js
+++ src/components/Timer/TimerTest.js
@@ -1,26 +1,34 @@
 import TagColor from "../../constants/TagColor";
 import TimerTime from "../../constants/TimerTime";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const TimerTest = ({ todo, isUpdateMode, emitClickedAction, action }) => {
 	const timerTitleRef = useRef();
-	const tagColorRef = useRef();
+	const [tagColor, setTagColor] = useState("");
 	const focusTimeRef = useRef();
 	const restTimeRef = useRef();
 
 	const onClickButton = (action) => {
 		const currentTitle = timerTitleRef.current.value;
-		const currentTagColor = tagColorRef.current.value;
+		const currentTagColor = tagColor;
 		const currentFocusTime = focusTimeRef.current.value;
 		const currentRestTime = restTimeRef.current.value;
 
 		emitClickedAction(action, { currentTitle, currentTagColor, currentFocusTime, currentRestTime });
 	};
 
+	const radioInputHandle = (tag) => {
+		setTagColor(() => {
+			return tag.name;
+		});
+	};
+
 	useEffect(() => {
 		if (todo) {
 			timerTitleRef.current.value = todo.title;
-			tagColorRef.current.value = todo.color;
+			setTagColor(() => {
+				return todo.color;
+			});
 			focusTimeRef.current.value = todo.focusTime;
 			restTimeRef.current.value = todo.restTime;
 		}
@@ -48,11 +56,17 @@ const TimerTest = ({ todo, isUpdateMode, emitClickedAction, action }) => {
 				</div>
 
 				<span>색상</span>
-				<div className="flex justify-around">
+				<div className="flex justify-evenly">
 					{TagColor.map((tag) => {
 						return (
 							<div key={tag.code}>
-								<input type="radio" id={tag.code} ref={tagColorRef} className="" />
+								<input
+									type="radio"
+									name="tagColor"
+									id={tag.code}
+									value={tag.name}
+									onChange={() => radioInputHandle(tag)}
+								/>
 								<label htmlFor={tag.code}> {tag.name}</label>
 							</div>
 						);
@@ -63,11 +77,11 @@ const TimerTest = ({ todo, isUpdateMode, emitClickedAction, action }) => {
 					<label htmlFor="focusTime" className="absolute">
 						FocusTime
 					</label>
-					<input id="focusTime" type="number" min="10" max="60" step="5" className="text-9xl" ref={focusTimeRef} />
+					<input id="focusTime" type="number" min="5" max="60" step="5" className="text-9xl" ref={focusTimeRef} />
 					<label htmlFor="restTime" className="absolute">
 						RestTime
 					</label>
-					<input id="restTime" type="number" min="10" max="60" step="5" className="text-9xl" ref={restTimeRef} />
+					<input id="restTime" type="number" min="5" max="60" step="5" className="text-9xl" ref={restTimeRef} />
 				</div>
 
 				<button
@@ -80,7 +94,7 @@ const TimerTest = ({ todo, isUpdateMode, emitClickedAction, action }) => {
 				<button
 					type="button"
 					className="w-40 h-10  border-slate-300 rounded-md mb-2 my-4 text-white bg-black"
-					onClick={(e) => onClickButton("delete")}
+					onClick={(e) => onClickButton(action)}
 				>
 					{action}
 				</button>
