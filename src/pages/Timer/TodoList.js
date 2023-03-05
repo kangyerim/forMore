d65c772e667c5820001523744a6986e285a926bd
강예림 <yerimkang521@gmail.com>
2023-03-06 00:20:31 +0900

    [fix] 생성, 수정 화면 나누는 것 작업중

--- src/pages/Timer/TodoList.js
+++ src/pages/Timer/TodoList.js
@@ -6,70 +6,84 @@ import TimerPage from "./TimerPage";
 const TodoList = () => {
 	const { getTodoList } = useTodosCollection();
 	const userInfo = useSelector((state) => state.authenSlice);
-	const [clickedTodo, setClickedTodo] = useState({});
 	const [todoList, setTodoList] = useState([]);
-	const [isUpdateMode, setUpdateMode] = useState(false);
+	const [isUpdateMode, setUpdateMode] = useState(true);
+	const [action, setAction] = useState("");
 
 	useEffect(() => {
 		if (userInfo.email) {
 			getTodoList().then((result) => {
-				const firstTodo = result[0];
-				setClickedTodo(() => {
-					return { ...firstTodo };
-				});
-				const restTodoList = result.splice(1, result.length);
 				setTodoList(() => {
-					return [...restTodoList, { title: "+", color: "grey", uid: String(new Date()) }];
+					return [...result, { title: "+", color: "grey", uid: String(new Date()) }];
 				});
 			});
 		}
 	}, [userInfo]);
 
 	const changeOrder = (event, index) => {
-		console.log("˚₊·—̳͟͞͞♡  event, index", event, index);
-		// let currentList = todoList.splice(0, todoList.length - 1);
+		const textContent = event.target.textContent;
+		if (textContent === "+") {
+			setAction(() => {
+				return "create";
+			});
+			const temp = todoList.splice(0, todoList.length - 1);
+			setTodoList(() => {
+				return [{ title: "", focusTime: 25, restTime: 5, uid: String(new Date()) }, ...temp];
+			});
 
-		// let front = currentList.splice(index, currentList.length);
-		// let back = currentList.splice(0, index);
+			return setUpdateMode(() => {
+				return !isUpdateMode;
+			});
+		}
+		let currentList = todoList.splice(0, todoList.length - 1);
 
-		// setTodoList(() => {
-		// 	return [...front, ...back, { title: "+", color: "grey", uid: String(new Date()) }];
-		// });
+		let front = currentList.splice(index, currentList.length);
+		let back = currentList.splice(0, index);
+		setTodoList(() => {
+			return [...front, ...back, { title: "+", color: "grey", uid: String(new Date()) }];
+		});
 	};
 
 	const changeMode = () => {
-		console.log("˚₊·—̳͟͞͞♡  changeMode");
+		setUpdateMode(() => {
+			return !isUpdateMode;
+		});
 	};
 
 	return (
 		<>
 			<div className="flex items-center">
-				<div className={`w-4/5 h-screen bg-${clickedTodo.color}-200 flex justify-center relative`}>
-					<h1 className="h-fit top-3 text-2xl font-bold align-middle absolute ">{clickedTodo.title}</h1>
-					<div className="absolute top-20">
-						<div>
-							<button onClick={changeMode}>수정</button>
-						</div>
-						<TimerPage todo={clickedTodo} />
-						<div>
-							<span className="text-2xl font-bold"> {clickedTodo.focusTime}/</span>
-							<span className="text-2xl font-bold">{clickedTodo.restTime}</span>
-						</div>
-					</div>
-				</div>
-
-				{todoList.map((todo, todoIndex) => {
-					return (
-						<div className={`w-20 h-screen bg-${todo.color}-200 flex items-center justify-center`} key={todoIndex}>
-							<h1
-								className="text-2xl font-bold align-middle whitespace-nowrap -rotate-90"
-								onClick={(e) => changeOrder(e, todoIndex)}
-							>
-								{todo.title}
-							</h1>
-						</div>
-					);
-				})}
+				{todoList &&
+					todoList.map((todo, index) => {
+						return index === 0 ? (
+							<div className={`w-4/5 z-10 h-screen bg-${todo.color}-200 flex relative`} key={todo.uid}>
+								{isUpdateMode ? (
+									<>
+										{" "}
+										<h1 className="h-fit top-20 text-7xl font-bold align-middle absolute ">{todo.title}</h1>
+										<div className="absolute bottom-20">
+											<span className="text-9xl font-bold"> {todo.focusTime}/</span>
+											<span className="text-9xl font-bold">{todo.restTime}</span>
+											<button className="underline" onClick={changeMode}>
+												수정
+											</button>
+										</div>
+									</>
+								) : (
+									<TimerPage todo={todo} isUpdateMode={isUpdateMode} action={action} onCancel={changeMode} />
+								)}
+							</div>
+						) : (
+							<div className={`w-20 h-screen bg-${todo.color}-200 flex items-center justify-center`} key={todo.uid}>
+								<h1
+									className="text-2xl font-bold align-middle whitespace-nowrap -rotate-90"
+									onClick={(e) => changeOrder(e, index)}
+								>
+									{todo.title}
+								</h1>
+							</div>
+						);
+					})}
 			</div>
 		</>
 	);
