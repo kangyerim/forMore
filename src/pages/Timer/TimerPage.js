b2ed3bf24413f75a4f649a4a6f189a0c3234ecb6
강예림 <yerimkang521@gmail.com>
2023-04-13 22:20:14 +0900

    [feat] 타이머 카운트 다운

--- src/pages/Timer/TimerPage.js
+++ src/pages/Timer/TimerPage.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const TimerPage = () => {
@@ -8,6 +8,7 @@ const TimerPage = () => {
 	const [focusTime, setFocusTime] = useState(-1);
 	const [restTime, setRestTime] = useState(-1);
 	const [mode, setMode] = useState("none"); // focus,rest,none
+	let countDownInterval = useRef(null);
 
 	useEffect(() => {
 		if (params) {
@@ -22,20 +23,25 @@ const TimerPage = () => {
 
 	const handleStart = () => {
 		setMode(() => "focus");
-		setInterval(() => {
-			setFocusTime((preTime) => {
-				if (preTime > 1) return preTime - 1;
-				else return 0;
-			});
-		}, 1000);
+		countDownInterval.current = setInterval(countDown, 1000);
+	};
+
+	const countDown = useCallback(() => {
+		setFocusTime((preTime) => {
+			if (preTime > 1) return preTime - 1;
+			else return 0;
+		});
+	}, []);
+
+	const handleStop = () => {
+		clearInterval(countDownInterval.current);
 	};
 
 	const formatedTimeByMinSecond = (time) => {
 		const timeByNumber = Number(time);
-		let min = Math.round(timeByNumber / 60);
-		let second = timeByNumber % 60;
+		let min = Math.floor(timeByNumber / 60);
+		let second = 10 > timeByNumber % 60 ? `0${timeByNumber % 60}` : timeByNumber % 60;
 
-		console.log("˚₊·—̳͟͞͞♡  timeByNumber:", Math.round(timeByNumber / 60), timeByNumber % 60);
 		return `${min}:${second}`;
 	};
 
@@ -45,13 +51,16 @@ const TimerPage = () => {
 
 	return (
 		<>
-			<h1>{todoName}</h1>
-			<h1>{formatedTimeByMinSecond(focusTime)}</h1>
-			<h1>{restTime}</h1>
+			<div className="py-10">
+				<h1>{todoName}</h1>
+				<h1>{formatedTimeByMinSecond(focusTime)}</h1>
+				<h1>{restTime}</h1>
 
-			<div>
-				<button onClick={handleStart}>start</button>
-				<button onClick={handleBack}>back</button>
+				<div>
+					<button onClick={handleStart}>start </button>
+					<button onClick={handleStop}>stop </button>
+					<button onClick={handleBack}>back</button>
+				</div>
 			</div>
 		</>
 	);
